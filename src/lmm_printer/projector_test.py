4f6f073ef104925466b1ebeb4bce87182c6b2f3f
Andrew Masker <andrewimasker@gmail.com>
2026-09-10 16:21:56 -0400

    Projector Test

--- src/lmm_printer/projector_test.py
+++ src/lmm_printer/projector_test.py
@@ -8,29 +8,34 @@ from lmm_printer.vendored.UV_projector.controller import DLPC1438, Mode
 
 GPIO.setmode(GPIO.BCM)
 
-# Initialize I2C (SMBus) on channel 1
-i2c = smbus.SMBus(1)
+try:
 
-# Initialise SPI (bus 0, with CE0 as chip select pin)
-spi = spidev.SpiDev()
-spi.open(0, 0)
-spi.max_speed_hz = 125000000  # FPGA/DCLP1438 limit: 50 MB/s; 125MHz seems limit for Pi zero 1W
-spi.mode = 3 
+    # Initialize I2C (SMBus) on channel 1
+    i2c = smbus.SMBus(1)
 
-# Initialise the DLPC1438
-DMD = DLPC1438(i2c, spi)
+    # Initialise SPI (bus 0, with CE0 as chip select pin)
+    spi = spidev.SpiDev()
+    spi.open(0, 0)
+    spi.max_speed_hz = 125000000  # FPGA/DCLP1438 limit: 50 MB/s; 125MHz seems limit for Pi zero 1W
+    spi.mode = 3 
+
+    # Initialise the DLPC1438
+    DMD = DLPC1438(i2c, spi)
 
 
-# let's try external print mode now
-DMD.configure_external_print(LED_PWM = 1000)
-DMD.switch_mode(Mode.EXTERNALPRINT)
+    # let's try external print mode now
+    DMD.configure_external_print(LED_PWM = 1000)
+    DMD.switch_mode(Mode.EXTERNALPRINT)
 
-# intialise FPGA buffers to zero
-DMD.set_background(intensity = 0, both_buffers = True)
+    # intialise FPGA buffers to zero
+    DMD.set_background(intensity = 0, both_buffers = True)
 
-# Send an image, where the raspberry pi determines the exposure time
-DMD.send_image_to_buffer('../media/openMLA_logo_1280x720.png', 0,0)  # send the image data into FPGA buffer over SPI
-DMD.swap_buffer()
-DMD.expose_pattern(exposed_frames = -1)  # infinite exposure time, until we send the stop_exposure() command
-time.sleep(4)
-DMD.stop_exposure()  # and then manually stop exposure
+    # Send an image, where the raspberry pi determines the exposure time
+    DMD.send_image_to_buffer('media/openMLA_logo_2560x1440.png', 0,0)  # send the image data into FPGA buffer over SPI
+    DMD.swap_buffer()
+    DMD.expose_pattern(exposed_frames = -1)  # infinite exposure time, until we send the stop_exposure() command
+    time.sleep(30)
+    DMD.stop_exposure()  # and then manually stop exposure
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
