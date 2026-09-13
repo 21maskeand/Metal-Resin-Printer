91a9628584d2fb2900eb1b9fda3dd84b6e276019
Andrew Masker <21maskeand@gmail.com>
2026-09-13 15:36:42 -0400

    PID Test

--- Teensy_Firmware/teensy_firmware/teensy_firmware.ino
+++ Teensy_Firmware/teensy_firmware/teensy_firmware.ino
@@ -18,7 +18,7 @@ void loop() {
   steppers_Update();
   pid_Update();
   
-  // Serial.print("Chamber at: "); Serial.print(return_Temp(0)); Serial.print(" of  "); Serial.print(return_Setpoint(0)); 
-  // Serial.print(", Recoater Heater at: "); Serial.print(return_Temp(1)); Serial.print(" of  "); Serial.print(return_Setpoint(1)); 
-  // Serial.print(", Recoater Front: "); Serial.print(return_Temp(2)); Serial.print(" of  "); Serial.println(return_Setpoint(1)); 
+  Serial.print("Chamber at: "); Serial.print(return_Temp(0)); Serial.print(" of  "); Serial.print(return_Setpoint(0)); 
+  Serial.print(", Recoater Heater at: "); Serial.print(return_Temp(1)); Serial.print(" of  "); Serial.print(return_Setpoint(1)); 
+  Serial.print(", Recoater Front: "); Serial.print(return_Temp(2)); Serial.print(" of  "); Serial.println(return_Setpoint(1)); 
 }
